5606a006a09d7fe230257c0266a04df797688d23
Yukito <yukito1989@gmail.com>
2016-09-29 14:16:37 +0700

    Sửa form main

--- src/vn/ChuVoiCon/qlHangHoa/DAO/chi_tiet_hoa_donDAO.java
+++ src/vn/ChuVoiCon/qlHangHoa/DAO/chi_tiet_hoa_donDAO.java
@@ -1,6 +1,7 @@
 package vn.ChuVoiCon.qlHangHoa.DAO;
 
 import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -9,19 +10,15 @@ import vn.ChuVoiCon.qlHangHoa.DLL.chi_tiet_hoa_don;
 import vn.ChuVoiCon.qlHangHoa.DLL.hoa_don;
 
 public class chi_tiet_hoa_donDAO extends Connect {
-	// Collum sql
 	private final String ma_hoa_don = "ma_hoa_don";
-	private final String IDHangHoa = "IDHangHoa";
+	private final String id_sp = "id_sp";
 	private final String don_gia = "don_gia";
 	private final String so_luong = "so_luong";
-	// Proc
-	private final String procGetHD = "chi_tiet_hoa_donGetHD(?)";
-	private final String procInsert = "chi_tiet_hoa_donInsert(?,?,?,?)";
 
 	protected chi_tiet_hoa_don convertData(ResultSet rs) throws SQLException {
 		chi_tiet_hoa_don r = new chi_tiet_hoa_don();
 		r.setMa_hoa_don(rs.getLong(ma_hoa_don));
-		r.setIDHangHoa(rs.getString(IDHangHoa));
+		r.setId_sp(rs.getString(id_sp));
 		r.setSo_luong(rs.getInt(so_luong));
 		r.setDon_gia(rs.getDouble(don_gia));
 		return r;
@@ -33,26 +30,30 @@ public class chi_tiet_hoa_donDAO extends Connect {
 	}
 	
 	public ArrayList<chi_tiet_hoa_don> getHD(hoa_don data) throws SQLException {
-		CallableStatement cstm = getCallableStatement(procGetHD);
-		cstm.setLong(ma_hoa_don, data.getMa_hoa_don());
-		ResultSet rs = cstm.executeQuery();
+		String sqlQuery = "SELECT cthd.*,sp.ten_sp";
+		sqlQuery += " FROM chi_tiet_hoa_don cthd INNER JOIN chi_tiet_san_pham sp on cthd.id_sp = sp.id_sp";
+		sqlQuery += " WHERE cthd.ma_hoa_don = ?";
+		PreparedStatement ptsm = getPreparedStatement(sqlQuery);
+		ptsm.setLong(1, data.getMa_hoa_don());
+		ResultSet rs = ptsm.executeQuery();
 		ArrayList<chi_tiet_hoa_don> r = new ArrayList<chi_tiet_hoa_don>();
 		while (rs.next()) {
 			r.add(convertData(rs,data));
 		}
 		rs.close();
-		cstm.close();
+		ptsm.close();
 		return r;
 	}
 
 	public int Insert(chi_tiet_hoa_don data) throws SQLException {
-		CallableStatement cstm = getCallableStatement(procInsert);
-		cstm.setLong(ma_hoa_don, data.getMa_hoa_don());
-		cstm.setString(IDHangHoa, data.getIDHangHoa());
-		cstm.setInt(so_luong, data.getSo_luong());
-		cstm.setDouble(don_gia, data.getDon_gia());
-		int r = cstm.executeUpdate();
-		cstm.close();
+		String sqlQuery = "INSERT INTO chi_tiet_hoa_don(ma_hoa_don,id_sp,so_luong,don_gia) VALUES(?,?,?,?)";
+		PreparedStatement ptsm = getPreparedStatement(sqlQuery);
+		ptsm.setLong(1, data.getMa_hoa_don());
+		ptsm.setString(2, data.getId_sp());
+		ptsm.setInt(3, data.getSo_luong());
+		ptsm.setDouble(4, data.getDon_gia());
+		int r = ptsm.executeUpdate();
+		ptsm.close();
 		return r;
 	}
 
